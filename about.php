d099f68a764e9808f7c06cc82135806ca803e757
laurynasn <lnavidau@gmail.com>
2014-05-26 15:44:38 -0400

    about added

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php include('doctop.php') ?>
+	<title>About Us</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+<?php include('docmid.php') ?>
+        
+    <h1>About Us</h1>
+
+    <p>We like beer and blogs.</p>
+        
+<?php include('docbottom.php') ?>
