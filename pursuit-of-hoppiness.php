48264d18981d684c0e5cf90f489cc6f9457b0182
dstirling <dstirlin@alumni.uoguelph.ca>
2014-05-26 20:43:14 -0400

    Edited blog all done

--- pursuit-of-hoppiness.php
+++ pursuit-of-hoppiness.php
@@ -13,8 +13,31 @@
 						<div class="pic-container post-image">
 							<!--<img src="images/local-taps.jpg" title="&copy The Local Gastropub" alt="The Local Gastropub's Taps" class="post-image" />-->
 						</div>
-						
+						<p></p>
+						<p>I like beer, you like beer, why else would you be here?  With  <a href="http://www.ocbweek.ca/" title="Ontario Craft Beer Week" target="_blank">Ontario Craft Beer Week</a> coming up it only seems appropriate to write about beer. My favorite beers are always a little (or a lot) hoppy, thus, my celebration of Craft Beer Week resides in trying every hop beer I can find.   In anticipation for the best week of the whole year I present to you: My Favorite Crafty Hop.</p>
+
 						<h2><a href="http://www.wellingtonbrewery.ca/index.php?Page=Our_Beers&Beer=3" title="Wellington Brewery" target="_blank">Arkell Best Bitter</a></h2>
+						<p>
+							Guelph has a lovely brewery by the name of Wellington Brewery.  This Brewery has never let me down; hops or not, if you are in Guelph go get yourself a Welly! Excuse me if I write passionately about this one and it makes you feel slightly uncomfortable but I can’t apologize for the nostalgia I have for this beer.</p>
+							<br />
+<h3>Wellington Arkell Best Bitter</h3>
+<br />
+<p>
+Once upon a time I lived downtown Guelph. When you live downtown Guelph you tend to get to know your beers. I have spent many afternoons and nights in Guelph trying their Wellys on.  My all-time favorite beer forever and ever is Arkell Best Bitter.</p>    <br /><br/>
+<h3>What makes it so good:</h3>
+<p>The combination of the sweet malt taste and hop finish.  It’s kind of a like a magic trick; you take a sip and you think “Where is the Best Bitter that the name promised”?  That’s where the trick starts: at first you taste its sweet malt flavor, making you think the name lied to you.  That draws you into the magic; you take another sip, again sweet malt, you set it down… then BAM; all that you knew that was sweet malt converts into a bitter smooth hop taste.  Magic. </p>
+
+<p>In case my analogy didn’t quite sell you on the idea….
+Arkell is a light beer, dry beer, with minimal carbonation and is hand-pulled if you get it on tap.   Of course it does come in bottles and cans,  but those contain more carbonation; so if you get the chance to sit down at [enter any venue that sells beer] in Guelph I recommend trying the cask Arkell Best Bitter to get a true sense of the authentic tastes that come out of Wellington Brewery.  Not that I am dissing bottles, their added carbonation adds an extra ‘refreshing’ sense; I just prefer cask over bottle.   
+
+						</p>
+
+<h2><a href="http://les3brasseurs.ca/eng/beers/3" title="The 3 Brewers" target="_blank">Honorable Mention: The 3 Brewers Blonde</a></h2>
+<br />
+<p>Since I’ve been in school I have to admint that my beer drinking has been to a minimum (sad, I know).  But I recently took a trip to The 3 Brewers in Toronto and tried their Blonde and it did not disappoint. 
+</p>    <br /><br/>
+<h3>What makes it so good:</h3>
+<p>It is a medley of aromatic hops.  I found it to be smooth with spice.   It reminded me of my favorite Best Bitter; this beer has a sweet taste and a bitter finish.  It has a bit of a fruity taste but it isn’t too overpowering and is cut beautifully by the bitter hop end.  I would say the only disappointment was that the hop end could have been a bit stronger, but that is because at the time I was in the mood for a strong hop beer.  If you like a light taste of hops, this beer is for you.  </p>
 
 				</article>
 <?php include('comments.php') ?>
