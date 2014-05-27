6879694999686742a640854efbaab47a36e3450a
linventive <william.j.lin@gmail.com>
2014-05-27 11:15:02 -0400

    99 bottles
    yo yo yo

---
+++ 99-bottles-but-gluten-in-none.php
@@ -0,0 +1,53 @@
+<?php include('doctop.php') ?>
+	<title>Hoppiness</title>
+	<meta name="description" content="One man's journey of exploration through the world of Gluten-Free Beer" />
+	<meta name="keywords" content="Gluten, Celiac, Coeliac, FODMAP, Fermentable Oligo Di Mono-saccharides And Polyols, Nickelbrook, beverage, LCBO" />
+	<meta name="author" content="Will Lin" />
+<?php include('docmid.php') ?>
+
+<article class="blog-post">
+	<header>
+		<h1>I Got 99 Bottles, But Gluten In None</h1>
+		<p class="author-date">By: Will Lin on 14/05/27</p>
+	</header>
+		<div class="pic-container post-image">
+			<!--<img src="images/local-taps.jpg" title="&copy The Local Gastropub" alt="The Local Gastropub's Taps" class="post-image" />-->
+		</div>
+	<p>Ok you lucky boys and girls.  I'm going to be taking one (or two) for the team today by sampling and reviewing some gluten-free options.  Most people of the gluten-free eaters (myself included) absolutely hate their beer options.  I don’t have celiac/coeliac disease or IBS, but I do have a weird gluten insensitivity that has a tendency to cause a pretty unpleasant joint inflammation that can mess with my mobility sometimes.  Recently, people haven’t been able to resist sending me <a href="http://www.huffingtonpost.ca/2014/05/14/gluten-intolerance-fake_n_5327420.html" target="_blank" />this article about how gluten intolerance may actually not exist</a>.  That article may be true, but it doesn’t really change my eating habits.  Saying I stick to a gluten-free diet is a lot easier than telling people that I’m sensitive to Fermentable, Oligo-, Di-, Mono-saccharides And Polyols (FODMAPs).  Semantics.</p>
+
+	<figure class="pic-container">
+		<img src="images/nickelbrook-bottles.jpg" title="Nickelbrook Gluten Free Alcoholic Beverage" alt="Nickelbrook Gluten Free Alcoholic Beverage"/>
+		<figcaption>A penny for your thoughts, a Nickel for your thirst.</figcaption>
+	</figure>
+
+
+	<p>So, I popped over to the local LCBO and was actually surprised by what I found.  One, there were a total of six gluten free beer options!  I live somewhat near one of the more sketchy parts of town, and it made me laugh to think about little thugs and gangbangers sipping away at gluten free beers.  The second thing that surprised me was that there weren’t just a few bottles or cans of each.  There were TONS – at least several dozens of each.  I looked online and discovered there were 241 cans of <a href="http://www.nickelbrook.com/our-beer/" target="_blank">Nickelbrook Gluten Free <del>Beer</del> <ins>Alcoholic Beverage</ins></a> at that branch.  By comparison, there were only about 100 cans of Molson Canadian.  So, I picked up a quartet of Nickelbrook and here I am.</p>
+
+	<ul class="ulpost">
+		<li>Nickelbrook Gluten Free Alcoholic Beverage. 5.8% alcohol. </li>
+		<li>Ingredients: Spring Water, Sorghum, Pear Juice, Demerara Sugar, Hops.</li>
+	</ul>
+
+	<p>The first sip is a strange one.  Heck, even the first sniff is unexpected.  I could smell and taste the pear.  I suppose it’s the fruit that gives it the cider-y taste.  My first reaction was to double check the can to make sure I didn’t buy a cider.  Before I forget, I should mention that this isn’t technically a beer, and thus they call it an alcoholic beverage.  To be considered a beer, Health Canada requires some sort of barley or wheat content.  Again, semantics.</p>
+
+	<p>Although my first taste of this beer was admittedly confused, there have been times when I’ve started a night being apprehensive  a certain beer only to absolutely love it on my 3rd or 4th round… so, here’s how it went.</p>
+
+	<figure class="pic-container figright">
+		<img src="images/nickelbrook-can.jpg" title="Nickelbrook Gluten Free Alcoholic Beverage" alt="Nickelbrook Gluten Free Alcoholic Beverage in a can" />
+		<figcaption>A penny for your thoughts, a Nickel for your thirst.</figcaption>
+	</figure>
+	<ul class="ulpost">
+		<li>1st Round:  Hmm.  Is it beer?  Is it cider?  Either option is good, but being halfway between is kind of confusing… but in a good way.</li>   
+		<li>2nd round:  For some reason I keep thinking of IKEA.  Why?  I’m not sure.  </li>
+		<li>3rd round:  Drank from a glass instead of a can this time.  It’s got a pleasant colour and carbonation to it, and if I close my eyes I can imagine I’m drinking a wacky ginger ale.  I now realize that the can is blue and yellow, similar to the Ikea logo.</li>
+		<li>4th round:  I’ll admit that by my fourth can, Nickelbrook has grown on me.  It’s not bad.  I imagine that this is probably exactly what some people are looking for in a beer.  I don’t think it would be my first choice, but I’m comfortable with it as an alternative.</li>
+	</ul>
+	<p>So, to sum up… given the option of having a Nickelbrook or any glass of wine, I’ll probably go for the wine.  However, given the option of a Nickelbrook or a Coors Light (or a Heiniken), I’ll take the Nickelbrook every.single.time.  </p>
+
+	<p>If you enjoyed the read, leave a comment.  In the coming weeks, I plan to review La Messagère, a gluten-free beer from Quebec. Also, Ontario Craft Beer Week 2014 is coming up!  With any luck I’ll be able to write about Snowman Brewing’s gluten free selection in the coming weeks!</p>
+
+	<p>*The title is a play on Jay-Z's epic track 99 Problems.</p>
+
+</article>
+<?php include('comments.php') ?>
+<?php include('docbottom.php') ?>
