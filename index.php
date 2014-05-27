7dbd650d4c0211f0c201f14999b3ccd7a3d60db8
linventive <william.j.lin@gmail.com>
2014-05-27 12:38:50 -0400

    99 bottles blurb

--- index.php
+++ index.php
@@ -60,14 +60,14 @@
 
 				<article class="recent-post">
 					<header>
-						<h3><a href="#">BLOG 5</a></h3>
-						<p class="author-date">By: Author on 11/11/11</p>
+						<h3><a href="99-bottles-but-gluten-in-none.php">I Got 99 Bottles But Gluten In None</a></h3>
+						<p class="author-date">By: Will on 27/05/2014</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/placeholder_16_9_md.jpg" title="Beer" alt="Beer" class="recent-image" />
+							<img src="images/nickelbrook-feature.jpg" title="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" alt="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" class="recent-image" />
 						</div>
-						<p class="recent-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis tellus orci, et porttitor massa aliquam eget. Nulla elementum sodales nulla, eu faucibus dolor suscipit placerat. Nulla non eleifend lorem. Etiam pulvinar lorem vitae convallis molestie. </p>
+						<p class="recent-text">Have you ever wondered about what gluten free beer is like?  Read all about Will’s first foray into the wild, wacky, world of gluten free alcoholic beverages.  First stop, Nickelbrook’s Gluten Free, a menagerie of pear, sorghum, Demerara sugar, and hops.  Bottoms up! </p>
 						<a href="#" class="comments-link">'x' Comments</a>
-						<a href="#" class="more-link">Read More...</a>
+						<a href="99-bottles-but-gluten-in-none.php" class="more-link">Read More...</a>
 				</article>
 <?php include('docbottom.php') ?>				
