9937229f746a0f0a45b7715342317e66d24ffc50
JoelLaCroix <joel.lacroix1@gmail.com>
2014-05-27 11:25:10 -0400

    fix format

--- at-brews-end.php
+++ at-brews-end.php
@@ -10,23 +10,23 @@
 <h1>At Brew's End</h1>
 <p class="author-date">By: Joel</p>
 </header>
-<p> My favourite movie of 2013 was most definitely  <a href="http://www.imdb.com/title/tt1213663/">The World’s End. </p> 
+<p> My favourite movie of 2013 was most definitely  <a href="http://www.imdb.com/title/tt1213663/">The World's End.</a> </p> 
 
-<p>In this film, a group of old buddies get together to re-attempt the epic pub crawl of their youth, “The Golden Mile” – a feat that they never completed:  five friends, twelve pubs, twelve pints, one night. During the attempt, they begin to notice something strange about their old hometown.  As they progress past each pub, the mystery unfolds and a zany adventure ensues.</p>
+<p>In this film, a group of old buddies get together to re-attempt the epic pub crawl of their youth, "The Golden Mile"- a feat that they never completed:  five friends, twelve pubs, twelve pints, one night. During the attempt, they begin to notice something strange about their old hometown.  As they progress past each pub, the mystery unfolds and a zany adventure ensues.</p>
 
-<p> Watching that movie inspired me to attempt something similar, with a slight twist. Seeing as how i don't drink (Ironic isn't it, I am writing a blog post about doing a pub crawl on a blog about craft beer and I don't drink lol.) I will be acting as the designated driver (we'll most likly be walking, but you get the idea) / Videographer for the night. (why? <em>reasons</em>)  I’m not going to be so ambitious as to attempt a 12-bar pub crawl, but I think 5 sounds like good fun, and more my speed. And it just so happens that in two short weeks, Ontario Craft Beer Week 2014 begins! </p>
+<p> Watching that movie inspired me to attempt something similar, with a slight twist. Seeing as how i don't drink (Ironic isn't it, I am writing a blog post about doing a pub crawl on a blog about craft beer and I don't drink lol.) I will be acting as the designated driver (we'll most likly be walking, but you get the idea) / Videographer for the night. (why? reasons)  I'm not going to be so ambitious as to attempt a 12-bar pub crawl, but I think 5 sounds like good fun, and more my speed. And it just so happens that in two short weeks, Ontario Craft Beer Week 2014 begins! </p>
 
 <p>So here's our plan for June 14</p>
 
 <ol>
 	<li>
-	<h2> Fanny’s Flights & Bites</h2>
-	<p>We begin our adventure with lunch at Fanny Chadwick’s on 268 Howland Ave, M5R3B7, where our meal will begin with bite-sized appetizers made with beer to go with their selection of available Ontario craft beers to wash it all down!</p>
+	<h2> Fanny's Flights & Bites</h2>
+	<p>We begin our adventure with lunch at Fanny Chadwick's on 268 Howland Ave, M5R3B7, where our meal will begin with bite-sized appetizers made with beer to go with their selection of available Ontario craft beers to wash it all down!</p>
 	</li>
 
 	<li>
 	<h2> The Tom Green Beer Signing with Tom Green @ Summerhill LCBO  </h2>
-	<p>The second stop is the LCBO at 10 Scivener Square Toronto, M4W 3Y9.  Here we will get a chance to meet the infamous comedian, Tom Green, and more importantly, sample the beer he’s collaborated on (a collaBEERation!). </p>
+	<p>The second stop is the LCBO at 10 Scivener Square Toronto, M4W 3Y9.  Here we will get a chance to meet the infamous comedian, Tom Green, and more importantly, sample the beer he's collaborated on (a collaBEERation!). </p>
 	</li>
 	
 	<li>
@@ -36,12 +36,12 @@
 	
 	<li>
 	<h2>GRIFFIN Session at Dundas Square</h2>
-	<p>Session Toronto 2014 is the main event of our little Craft Beer Fest crawl.  Here, we’ll be able to sample 100+ craft beers and unique collaborations!  In addition we’ll be seeing savory delights of Bacon Nation there, along with more live music.  The party ends at 9pm, so there’s no time to waste!</p>
+	<p>Session Toronto 2014 is the main event of our little Craft Beer Fest crawl.  Here, we'll be able to sample 100+ craft beers and unique collaborations!  In addition we'll be seeing savory delights of Bacon Nation there, along with more live music.  The party ends at 9pm, so there’s no time to waste!</p>
 	</li>
 	
 	<li>
 	<h2>Social Saturdays at the Mill Street Brew Pub</h2>
-	<p>If we’re still standing at the end of the day, there’s no better place to finish off our adventure than in the Mill St Brew Pub in Toronto’s Historic Distillery District.  Take in the picturesque atmosphere, cool our sunburns, and relax with our beer buddies after a fantastic day of craft beer and summer cheer.  </p>
+	<p>If we're still standing at the end of the day, there's no better place to finish off our adventure than in the Mill St Brew Pub in Toronto's Historic Distillery District.  Take in the picturesque atmosphere, cool our sunburns, and relax with our beer buddies after a fantastic day of craft beer and summer cheer.  </p>
 	</li>
 	
 	<br />
