6b6e6fcfb53e775ba5313a5b189698f057b3dd65
dstirling <dstirlin@alumni.uoguelph.ca>
2014-05-27 11:29:45 -0400

    adding to index

--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 
 				<article class="recent-post">
 					<header>
-						<h3><a href="#">BLOG 2 </a></h3>
-						<p class="author-date">By: Author on 11/11/11</p>
+						<h3><a href="pursuit-of-hoppiness.php">BLOG 2 </a></h3>
+						<p class="author-date">By: Danielle Stirling on 25/05/20</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/placeholder_16_9_md.jpg" title="Beer" alt="Beer" class="recent-image" />
+							<img src="images/dee_beer.jpg" title="Beer" alt="Beer" class="recent-image" />
 						</div>
-						<p class="recent-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis tellus orci, et porttitor massa aliquam eget. Nulla elementum sodales nulla, eu faucibus dolor suscipit placerat. Nulla non eleifend lorem. Etiam pulvinar lorem vitae convallis molestie. </p>
+						<p class="recent-text">With  <a href="http://www.ocbweek.ca/" title="Ontario Craft Beer Week" target="_blank">Ontario Craft Beer Week</a> coming up it only seems appropriate to write about beer. My favorite beers are always a little (or a lot) hoppy, thus, my celebration of Craft Beer Week resides in trying every hop beer I can find.   In anticipation for the best week of the whole year I present to you: My Favorite Crafty Hop.</p>
 						<a href="#" class="comments-link">'x' Comments</a>
 						<a href="#" class="more-link">Read More...</a>
 				</article>
