d2e6fc44a8af91c23c44ebe2600cf3b178046ecc
laurynasn <lnavidau@gmail.com>
2014-05-28 14:13:10 -0400

    styling changes; linking homepage images to posts

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 						<p class="author-date">By: Stacey Masson on 25/05/14</p>
 					</header>
 						<div class="pic-container featured-image">
-							<img src="images/the-local-gastropub.jpg" title="&copy; The Local Gastropub" alt="The Local Gastropub" class="recent-image" />
+							<a href="best-craft-pubs" title="The Best Craft Beer Hotspots in Toronto and Beyond"><img src="images/the-local-gastropub.jpg" title="&copy; The Local Gastropub" alt="The Local Gastropub" class="recent-image" /></a>
 						</div>
 						<p class="featured-text">Can’t get enough craft beer, and want to get straight to the source?  Read ahead to find out where in Toronto (and beyond!) to go to quench that thirst!</p>
 						<!--<a href="#" class="comments-link">'x' Comments</a>-->
@@ -25,7 +25,7 @@
 						<p class="author-date">By: Danielle Stirling on 25/05/2014</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/dee_beer.jpg" title="Beer" alt="Beer" class="recent-image" />
+							<a href="pursuit-of-hoppiness" title="Pursuit of Hoppiness"><img src="images/dee_beer.jpg" title="Beer" alt="Beer" class="recent-image" /></a>
 						</div>
 						<p class="recent-text">With  <a href="http://www.ocbweek.ca/" title="Ontario Craft Beer Week" target="_blank">Ontario Craft Beer Week</a> coming up it only seems appropriate to write about beer. My favorite beers are hoppy, thus, my celebration of Craft Beer Week resides in trying hop beers.  In anticipation for the best week of the whole year I present to you: My Favorite Crafty Hop.</p>
 						<!--<a href="#" class="comments-link">'x' Comments</a>-->
@@ -38,7 +38,7 @@
 						<p class="author-date">By: Joel on 25/05/2014</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/brews_end.jpg" title="Beer" alt="Beer" class="recent-image" />
+							<a href="at-brews-end" title="At Brew's End"><img src="images/brews_end.jpg" title="Beer" alt="Beer" class="recent-image" /></a>
 						</div>
 						<p class="recent-text">Five friends, five pubs, five pints?, one night. Join us as we crawl form pub to pub in celebration of Toronto Craft Beer Week.</p>
 						<!--<a href="#" class="comments-link">'x' Comments</a>-->
@@ -51,7 +51,7 @@
 						<p class="author-date">By: Laurynas on 25/05/2014</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/beer_bread_recipe_01.jpg" title="Beer and cheddar bread" alt="Beerand cheddar bread" class="recent-image" />
+							<a href="beer-bread-recipe" title="Stout and Cheddar Bread Recipe"><img src="images/beer_bread_recipe_01.jpg" title="Beer and cheddar bread" alt="Beerand cheddar bread" class="recent-image" /></a>
 						</div>
 						<p class="recent-text">Want something savoury to enjoy with your local craft pint? Try baking this beer and cheddar bread.</p>
 						<!--<a href="#" class="comments-link">'x' Comments</a>-->
@@ -64,7 +64,7 @@
 						<p class="author-date">By: Will on 27/05/2014</p>
 					</header>
 						<div class="pic-container recent-image">
-							<img src="images/nickelbrook-feature.jpg" title="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" alt="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" class="recent-image" />
+							<a href="99-bottles-but-gluten-in-none" title="I Got 99 Bottles But Gluten In None"><img src="images/nickelbrook-feature.jpg" title="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" alt="Nickelbrook's selection of beverages, featuring their Gluten Free Alcoholic Beverage" class="recent-image" /></a>
 						</div>
 						<p class="recent-text">Have you ever wondered about what gluten free beer is like?  Read all about Will’s first foray into the wild, wacky, world of gluten free alcoholic beverages.  First stop, Nickelbrook’s Gluten Free, a menagerie of pear, sorghum, Demerara sugar, and hops.  Bottoms up! </p>
 						<!--<a href="#" class="comments-link">'x' Comments</a>-->
