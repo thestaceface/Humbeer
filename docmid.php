bbdc409549ddfcc3b539f46781e2e0f539c02668
laurynasn <lnavidau@gmail.com>
2014-05-28 10:17:43 -0400

    added analytics

--- docmid.php
+++ docmid.php
@@ -6,6 +6,16 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="ce/commentEngine.js"></script>
         <script src="ce/contactForm.js"></script>
+        <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-51383612-1', 'brewhahablog.com');
+	  ga('send', 'pageview');
+	
+	</script>
 </head>
 <body>
 
