4541cd1b55c521d0154adc13bbae8a31d2d1eb91
linventive <william.j.lin@gmail.com>
2014-05-28 23:02:10 -0400

    99 typos and grammatical errors

--- 99-bottles-but-gluten-in-none.php
+++ 99-bottles-but-gluten-in-none.php
@@ -11,7 +11,7 @@
 		<p class="author-date">By: Will Lin on 25/05/2014</p>
 	</header>
 
-	<p>Ok you lucky boys and girls.  I'm going to be taking one (or two) for the team today by sampling and reviewing some gluten-free options.  Most people of the gluten-free eaters (myself included) absolutely hate their beer options.  I don’t have celiac/coeliac disease or IBS, but I do have a weird gluten insensitivity that has a tendency to cause a pretty unpleasant joint inflammation that can mess with my mobility sometimes.  Recently, people haven’t been able to resist sending me <a href="http://www.huffingtonpost.ca/2014/05/14/gluten-intolerance-fake_n_5327420.html" target="_blank" >this article about how gluten intolerance may actually not exist</a>.  That article may be true, but it doesn’t really change my eating habits.  Saying I stick to a gluten-free diet is a lot easier than telling people that I’m sensitive to Fermentable, Oligo-, Di-, Mono-saccharides And Polyols (FODMAPs).  Semantics.</p>
+	<p>Ok you lucky boys and girls.  I'm going to be taking one (or two) for the team today by sampling and reviewing some gluten-free options.  Most of the gluten-free eaters that I know (myself included) absolutely hate their beer options.  I don’t have celiac/coeliac disease or IBS, but I do have a weird gluten insensitivity.  Eating gluten-heavy foods has a tendency to cause a pretty unpleasant joint inflammation that can mess with my mobility sometimes.  Recently, people haven’t been able to resist sending me <a href="http://www.huffingtonpost.ca/2014/05/14/gluten-intolerance-fake_n_5327420.html" target="_blank" >this article about how gluten intolerance may actually not exist</a>.  That article may be true, but it's not really going change my eating habits or my naming of it.  Saying I stick to a gluten-free diet is a lot easier than telling people that I’m sensitive to Fermentable, Oligo-, Di-, Mono-saccharides And Polyols (FODMAPs).  Semantics.</p>
 
 	<figure class="pic-container">
 		<img src="images/nickelbrook-bottles.jpg" title="Nickelbrook Gluten Free Alcoholic Beverage" alt="Nickelbrook Gluten Free Alcoholic Beverage"/>
@@ -19,16 +19,16 @@
 	</figure>
 
 
-	<p>So, I popped over to the local LCBO and was actually surprised by what I found.  One, there were a total of six gluten free beer options!  I live somewhat near one of the more sketchy parts of town, and it made me laugh to think about little thugs and gangbangers sipping away at gluten free beers.  The second thing that surprised me was that there weren’t just a few bottles or cans of each.  There were TONS – at least several dozens of each.  I looked online and discovered there were 241 cans of <a href="http://www.nickelbrook.com/our-beer/" target="_blank">Nickelbrook Gluten Free <del>Beer</del> <ins>Alcoholic Beverage</ins></a> at that branch.  By comparison, there were only about 100 cans of Molson Canadian.  So, I picked up a quartet of Nickelbrook and here I am.</p>
+	<p>So, I popped over to the local LCBO and was actually surprised by what I found.  One, there were a total of six gluten free beer options!  I live somewhat near one of the more sketchy parts of town and it made me laugh to think about little thugs and gangbangers sipping away at gluten free beers.  The second thing that surprised me was that there weren’t just a few bottles or cans of each.  There were TONS – at least several dozens of each.  I looked online and discovered there were 241 cans of <a href="http://www.nickelbrook.com/our-beer/" target="_blank">Nickelbrook Gluten Free <del>Beer</del> <ins>Alcoholic Beverage</ins></a> at that branch.  By comparison, there were only about 100 cans of Molson Canadian.  So, I picked up a quartet of Nickelbrook and here I am.</p>
 
 	<ul class="ulpost">
 		<li>Nickelbrook Gluten Free Alcoholic Beverage. 5.8% alcohol. </li>
 		<li>Ingredients: Spring Water, Sorghum, Pear Juice, Demerara Sugar, Hops.</li>
 	</ul>
 
-	<p>The first sip is a strange one.  Heck, even the first sniff is unexpected.  I could smell and taste the pear.  I suppose it’s the fruit that gives it the cider-y taste.  My first reaction was to double check the can to make sure I didn’t buy a cider.  Before I forget, I should mention that this isn’t technically a beer, and thus they call it an alcoholic beverage.  To be considered a beer, Health Canada requires some sort of barley or wheat content.  Again, semantics.</p>
+	<p>The first sip is a strange one.  Heck, even the first sniff is unexpected.  I could smell and taste the pear.  I suppose it’s the fruit that gives it the cider-y taste.  My first reaction was to double check the can to reconfirm that I didn’t actually accidentally buy a cider.  Before I forget, I should mention that this isn’t technically a beer, and thus they call it an alcoholic beverage.  To be considered a beer, Health Canada requires some sort of barley or wheat content.  Again, semantics.</p>
 
-	<p>Although my first taste of this beer was admittedly confused, there have been times when I’ve started a night being apprehensive  a certain beer only to absolutely love it on my 3rd or 4th round… so, here’s how it went.</p>
+	<p>Although my first taste of this beer left me with an admittedly confused feeling, there have been times when I’ve started a night being apprehensive about a certain beer only to absolutely love it on my 3rd or 4th round… so, here’s how it went.</p>
 
 	<figure class="pic-container figright">
 		<img src="images/nickelbrook-can.jpg" title="Nickelbrook Gluten Free Alcoholic Beverage Can" alt="Nickelbrook Gluten Free Alcoholic Beverage in a can" />
